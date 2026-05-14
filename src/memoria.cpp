49fea00e436d75f2709ea7163823bd2f9f95df72
a25110256-lgtm <a25110256@ceti.mx>
2026-05-14 22:55:14 +0000

    actividad 5

--- src/memoria.cpp
+++ src/memoria.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main ()
+ {
     int hola; 
+
     cout<<&hola<<endl;
+
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(short)<<endl;
+    cout<<sizeof(long double)<<endl;
 
     return 0; 
 }
